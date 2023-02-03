24d86fa250f20db75bbf6c614c86e7a1c07afff9
MD SAHIDUL ISLAM <111642492+sahidul7@users.noreply.github.com>
2023-02-03 14:51:48 +0600

    Create A - Again Twenty Five!.cpp

---
+++ A - Again Twenty Five!.cpp
@@ -0,0 +1,19 @@
+/*
+A. Again Twenty Five!
+time limit per test0.5 seconds
+memory limit per test64 megabytes
+inputstandard input
+outputstandard output
+*/
+//By Sahidul_7, contest: Experimental Educational Round: VolBIT Formulas Blitz, problem: (A) Again Twenty Five!, Accepted;
+
+/* Author: Md Sahidul Islam */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    cout << 25 << endl;
+    return 0;
+}
