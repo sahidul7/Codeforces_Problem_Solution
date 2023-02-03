6fac22ea91960b741d160ea9d873a46986ee0079
MD SAHIDUL ISLAM <111642492+sahidul7@users.noreply.github.com>
2023-02-03 13:27:59 +0600

    Update A. Vasya and Socks.cpp

--- A. Vasya and Socks.cpp
+++ A. Vasya and Socks.cpp
@@ -1,4 +1,13 @@
+/*A. Vasya and Socks
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output*/
+
+
+
 //By Sahidul_7, contest: Codeforces Round #262 (Div. 2), problem: (A) Vasya and Socks, Accepted
+/*Author:Md Sahidul Islam*/
 
 #include <bits/stdc++.h>
 using namespace std;
