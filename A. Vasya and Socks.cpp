83d75c0db76bec29a6de1234c188283f0cf65b78
MD SAHIDUL ISLAM <111642492+sahidul7@users.noreply.github.com>
2023-02-03 13:22:33 +0600

    Create A. Vasya and Socks.cpp

---
+++ A. Vasya and Socks.cpp
@@ -0,0 +1,16 @@
+//By Sahidul_7, contest: Codeforces Round #262 (Div. 2), problem: (A) Vasya and Socks, Accepted
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int n,m;
+	cin>>n>>m;
+	int ans=(n-1)/(m-1);
+	cout<<n+ans;
+ 
+ 
+ 
+ 
+	return 0;
+}
