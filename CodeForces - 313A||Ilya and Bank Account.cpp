e7ac27f882457101a02520ba1ec611ce7721f95c
MD SAHIDUL ISLAM <111642492+sahidul7@users.noreply.github.com>
2023-03-02 02:27:23 +0600

    Create CodeForces - 313A||Ilya and Bank Account.cpp

---
+++ CodeForces - 313A||Ilya and Bank Account.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FILE   freopen("perfectly_balanced_chapter_1_input.txt", "r", stdin);  freopen("output.txt", "w", stdout);
+#define coutd cout<<fixed<<setprecision(10)//coutd<<res<<endl;
+#define endl "\n"
+using namespace std;
+int main(){
+    FAST;
+      int n;
+    cin >> n;
+    if (n >= 0)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        int a = n / 10;
+        int b = n / 100 * 10 + n % 10;
+      
+        cout << max(a, b) << endl;
+    }
+    return 0;
+    
+    
+}
+
+
+
